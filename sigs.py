69864d07cac61751e588f5e9c54c039da0822ec3
Matt Brooks <matt@citizenlab.ca>
2018-03-02 14:54:52 +0800

    Refactored sigs.py for parent/child Handler classes

--- sigs.py
+++ sigs.py
@@ -32,9 +32,9 @@ NOTES:
 
 TODO:
 
-- Implement the StringSetHandler class.
+- Implement dynamic instantiation based on file extension
+  in the Renamer class' rename_recognized_code method.
 
-- Redesign using inheritance from a parent class.
 
 '''
 
@@ -47,12 +47,58 @@ import base64
 
 import r2pipe
 
-class ZigHandler:
+class Renamer:
+
+    def __init__(self):
+        pass
+
+    def rename_recognized_code(self, infile):
+
+        # TODO: Implement dynamic instantiation based on file extension
+
+        if os.path.isdir(infile):
+            
+            for file in os.listdir(infile):
+                
+                if file.endswith('.zighashes'):
+
+                    zh = ZigHandler()
+                    zh.generate_hashes_from_session()
+                    zh.rename_session_functions(os.path.join(infile, file))
+
+                elif file.endswith('.stringsethashes'):
+
+                    ssh = StringSetHandler()
+                    ssh.generate_hashes_from_session()
+                    ssh.rename_session_functions(os.path.join(infile, file))
+
+        elif os.path.isfile(infile):
+            
+            if infile.endswith('.zighashes'):
+
+                zh = ZigHandler()
+                zh.generate_hashes_from_session()
+                zh.rename_session_functions(infile)
+
+            elif infile.endswith('.stringsethashes'):
+
+                ssh = StringSetHandler()
+                ssh.generate_hashes_from_session()
+                ssh.rename_session_functions(infile)
+
+class Handler:
 
     def __init__(self):
 
         self.hashes = {}
         self.temphashes = set()
+
+        self.EXTENSION = ''
+
+    def generate_hashes(self):
+        
+        #This is overridden by child classes
+        pass
 
     def generate_hashes_from_session(self):
 
@@ -60,16 +106,15 @@ class ZigHandler:
 
     def generate_hashes_from_input(self, infile, outfile):
 
-        # Execute based on inputs and output
         if os.path.isdir(infile) and self.check_outfile(outfile):
 
             self.generate_hashes_from_dir(infile)
-            self.write_zighash_file(outfile)
+            self.write_hash_file(outfile)
         
         elif os.path.isfile(infile) and self.check_outfile(outfile):
         
             self.generate_hashes(infile)
-            self.write_zighash_file(outfile)
+            self.write_hash_file(outfile)
         
         else:
         
@@ -80,40 +125,8 @@ class ZigHandler:
         
         for file in self.sorted_alphanumeric(os.listdir(directory)):
 
-            print "Generating zig hashes for " + os.path.join(directory,file)
-            self.generate_hashes(os.path.join(directory,file))
-
-    def generate_hashes(self, infile=None):
-        
-        r2 = r2pipe.open(infile)
-        r2.cmd('aa; aar; aac')
-
-        funcs = r2.cmdj('aflj')
-
-        for func in funcs:
-
-            # Generate the full zignature.
-            r2.cmd('zaf ' + func['name'] + ' ' + self.generate_func_name(func['name'],infile))
-
-            # View the zignature, get the bytes, and hash them.
-            zigs = r2.cmdj('zj')
-            zig_bytes = zigs[0]['bytes'] #Hacky but only a single sig can ever exist at once.
-            sig_byte_hash = hashlib.md5(zig_bytes.encode()).hexdigest()
-
-            # Only create if the function is not too short and the 
-            # function hash is not stored already
-            if len(zig_bytes) > 30 and sig_byte_hash not in self.temphashes:
-
-                # Add the func+hash pair to the dict
-                self.hashes[self.generate_func_name(func['name'],infile)] = sig_byte_hash
-
-                # Update set
-                self.temphashes.add(sig_byte_hash)
-
-            # Delete all zignatures to keep continuous assurance only one exists at a time.
-            r2.cmd('z-*')
-            
-        r2.quit()
+            print "Generating hashes for " + os.path.join(directory, file)
+            self.generate_hashes(os.path.join(directory, file))
 
     def rename_session_functions(self, infile):
         
@@ -121,30 +134,53 @@ class ZigHandler:
             
             for file in self.sorted_alphanumeric(os.listdir(infile)):
 
-                if file.endswith('.zighashes'):
+                if file.endswith(self.EXTENSION):
 
-                    print "Matching zig hashes for " + os.path.join(infile,file)
-                    self.rename_session_functions_from_zighash_file(os.path.join(infile,file))
+                    self.rename_session_functions_from_hash_file(
+                        os.path.join(infile,file)
+                    )
 
-        elif os.path.isfile(infile) and infile.endswith('.zighashes'):
+        elif os.path.isfile(infile) and infile.endswith(self.EXTENSION):
 
-            self.rename_session_functions_from_zighash_file(infile)
+            self.rename_session_functions_from_hash_file(infile)
 
-
-    def rename_session_functions_from_zighash_file(self, infile):
-
+    def rename_session_functions_from_hash_file(self, infile):
+        
         r2 = r2pipe.open()
         
         with open(infile,'r') as f:
                 file_hashes = json.load(f)
 
         for funcname, funchash in self.hashes.iteritems():
-            if funcname.startswith('fcn.') and funchash in set(file_hashes.values()):
+
+            if (funcname.startswith('fcn.') and 
+                funchash in set(file_hashes.values())):
 
                 r2.cmd('s ' + funcname)
-                r2.cmd('afn ' + self.generate_func_name(self.get_dict_key_from_value(file_hashes, funchash)))
+                r2.cmd('afn ' + self.generate_func_name(
+                    self.get_dict_key_from_value(file_hashes, funchash))
+                )
 
         r2.quit()
+
+    def write_hash_file(self, outfile):
+
+        if (outfile.count('.') == 1 and
+            outfile.endswith(self.EXTENSION)):
+
+            pass
+
+        elif outfile.count('.') == 0:
+
+            outfile = outfile + self.EXTENSION
+
+        elif ('.' in outfile and 
+            not outfile.endswith(self.EXTENSION)):
+
+            outfile = outfile.split('.')[0] + self.EXTENSION
+
+        with open(outfile,'w') as f:
+            json.dump(self.hashes, f)
 
     def check_outfile(self, outfile):
 
@@ -154,16 +190,9 @@ class ZigHandler:
         try:
             with open(outfile,'w') as outfile:
                 return True
+
         except IOError:
             return False
-
-    def write_zighash_file(self, outfile):
-
-        if not outfile.endswith('.zighashes'):
-            outfile = outfile + '.zighashes'
-
-        with open(outfile,'w') as f:
-            json.dump(self.hashes, f)
 
     def generate_func_name(self, name, infile=None):
 
@@ -176,59 +205,78 @@ class ZigHandler:
         else:
             return name
 
-
     # Taken from:
-    # https://stackoverflow.com/questions/4813061/nonalphanumeric-list-order-from-os-listdir-in-python
+    # https://stackoverflow.com/questions/4813061/
+    # nonalphanumeric-list-order-from-os-listdir-in-python
 
     def sorted_alphanumeric(self, data):
         convert = lambda text: int(text) if text.isdigit() else text.lower()
-        alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ] 
+        alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ]
         return sorted(data, key=alphanum_key)
 
-
     # Taken from:
-    # https://stackoverflow.com/questions/15784590/how-can-you-print-a-key-given-a-value-in-a-dictionary-for-python/15784656
+    # https://stackoverflow.com/questions/15784590/
+    # how-can-you-print-a-key-given-a-value-in-a-dictionary-for-python/15784656
 
     def get_dict_key_from_value(self, dict, value):
         return [k for k,v in dict.iteritems() if v == value][0]
 
 
-
-class StringSetHandler:
+class ZigHandler(Handler):
 
     def __init__(self):
 
-        self.hashes = {}
-        self.temphashes = set()
-    
-    def generate_hashes_from_session(self):
-        
-        self.generate_hashes()
+        Handler.__init__(self)
+        self.EXTENSION = '.zighashes'
 
-    def generate_hashes_from_input(self, infile, outfile):
+    def generate_hashes(self, infile=None):
         
-        # Execute based on inputs and output
-        if os.path.isdir(infile) and self.check_outfile(outfile):
+        r2 = r2pipe.open(infile)
+        r2.cmd('aa; aar; aac')
 
-            self.generate_hashes_from_dir(infile)
-            self.write_stringsethash_file(outfile)
-        
-        elif os.path.isfile(infile) and self.check_outfile(outfile):
-        
-            self.generate_hashes(infile)
-            self.write_stringsethash_file(outfile)
-        
-        else:
-        
-            print "\nCannot create from input.\n"
-            sys.exit(1)
+        funcs = r2.cmdj('aflj')
 
-    def generate_hashes_from_dir(self, directory):
-        
-        for file in self.sorted_alphanumeric(os.listdir(directory)):
+        for func in funcs:
 
-            print "Generating string set hashes for " + os.path.join(directory,file)
-            self.generate_hashes(os.path.join(directory,file))
+            # Generate the full zignature.
+            r2.cmd('zaf ' + func['name'] + ' ' + self.generate_func_name(
+                func['name'],infile)
+            )
+
+            # View the zignature, get the bytes, and hash them.
+            zigs = r2.cmdj('zj')
+
+            # Hacky but only a single sig can ever exist at once.
+            zig_bytes = zigs[0]['bytes'] 
+
+            sig_byte_hash = hashlib.md5(zig_bytes.encode()).hexdigest()
+
+            # Only create if the function is not too short and the 
+            # function hash is not stored already
+            if len(zig_bytes) > 30 and sig_byte_hash not in self.temphashes:
+
+                # Add the func+hash pair to the dict
+                self.hashes[self.generate_func_name(func['name'],infile)] = (
+                    sig_byte_hash
+                )
+
+                # Update set
+                self.temphashes.add(sig_byte_hash)
+
+            # Delete all zignatures to keep continuous 
+            # assurance only one exists at a time.
+            r2.cmd('z-*')
+            
+        r2.quit()
+
+
+
+class StringSetHandler(Handler):
+
+    def __init__(self):
+        
+        Handler.__init__(self)
+        self.EXTENSION = '.stringsethashes'
 
     def generate_hashes(self, infile=None):
 
@@ -252,9 +300,16 @@ class StringSetHandler:
                                 len(base64.b64decode(string['string'])) >= 10):
 
                                 if xref['fcn_name'] in string_sets:
-                                    string_sets[xref['fcn_name']].append(base64.b64decode(string['string']))
+
+                                    string_sets[xref['fcn_name']].append(
+                                        base64.b64decode(string['string'])
+                                    )
+
                                 elif xref['fcn_name'] not in string_sets:
-                                    string_sets[xref['fcn_name']] = [base64.b64decode(string['string'])]
+
+                                    string_sets[xref['fcn_name']] = (
+                                        [base64.b64decode(string['string'])]
+                                    )
 
 
         for func, stringset in string_sets.iteritems():
@@ -264,98 +319,11 @@ class StringSetHandler:
             if stringsethash not in self.temphashes:
 
                 self.temphashes.add(stringsethash)
-                self.hashes[self.generate_func_name(func, infile)] = stringsethash
-                #print func, stringsethash
-
-        r2.quit()
-
-    def rename_session_functions(self, infile):
-        
-        if os.path.isdir(infile):
-            
-            for file in self.sorted_alphanumeric(os.listdir(infile)):
-
-                if file.endswith('.stringsethashes'):
-
-                    print "Matching string set hashes for " + os.path.join(infile,file)
-                    self.rename_session_functions_from_stringsethash_file(os.path.join(infile,file))
-
-        elif os.path.isfile(infile) and infile.endswith('.stringsethashes'):
-
-            self.rename_session_functions_from_stringsethash_file(infile)
-
-    def rename_session_functions_from_stringsethash_file(self, infile):
-        
-        r2 = r2pipe.open()
-        
-        with open(infile,'r') as f:
-                file_hashes = json.load(f)
-
-        print 'file hashes are:'
-        for item in file_hashes.values():
-            print '\t' + item
-
-        for funcname, funchash in self.hashes.iteritems():
-
-            print "trying to match " + funcname, funchash
-
-            if funcname.startswith('fcn.') and funchash in set(file_hashes.values()):
-
-                print "\tmatching: ", funcname, funchash
-
-                r2.cmd('s ' + funcname)
-                r2.cmd('afn ' + self.generate_func_name(
-                    self.get_dict_key_from_value(file_hashes, funchash))
+                self.hashes[self.generate_func_name(func, infile)] = (
+                    stringsethash
                 )
 
         r2.quit()
-
-    def check_outfile(self, outfile):
-
-        if not outfile:
-            return False
-
-        try:
-            with open(outfile,'w') as outfile:
-                return True
-
-        except IOError:
-            return False
-
-    def write_stringsethash_file(self, outfile):
-
-        if not outfile.endswith('.stringsethashes'):
-            outfile = outfile + '.stringsethashes'
-
-        with open(outfile,'w') as f:
-            json.dump(self.hashes, f)
-
-    def generate_func_name(self, name, infile=None):
-
-        if not infile:
-            return name
-        elif name.startswith('sym.'):
-            return name
-        elif name.startswith('fcn.') or name.startswith('loc.'):
-            return str(os.path.basename(infile)) + '_' + name
-        else:
-            return name
-
-
-    # Taken from:
-    # https://stackoverflow.com/questions/4813061/nonalphanumeric-list-order-from-os-listdir-in-python
-
-    def sorted_alphanumeric(self, data):
-        convert = lambda text: int(text) if text.isdigit() else text.lower()
-        alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ] 
-        return sorted(data, key=alphanum_key)
-
-
-    # Taken from:
-    # https://stackoverflow.com/questions/15784590/how-can-you-print-a-key-given-a-value-in-a-dictionary-for-python/15784656
-
-    def get_dict_key_from_value(self, dict, value):
-        return [k for k,v in dict.iteritems() if v == value][0]
 
 
 if __name__ == '__main__':
@@ -363,15 +331,23 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     mode = parser.add_mutually_exclusive_group(required=True)
-    mode.add_argument('-g','--generate',action='store_true',help='Generate signatures for file or directory')
-    mode.add_argument('-m','--match',action='store_true',help='Match signatures for current session')
+
+    mode.add_argument('-g','--generate',
+        action='store_true',help='Generate signatures for file or directory')
+    mode.add_argument('-m','--match',
+        action='store_true',help='Match signatures for current session')
 
     sigtype = parser.add_mutually_exclusive_group(required=True)
-    sigtype.add_argument('-z','--zigs', action='store_true',help='Operate using r2 zignature format.')
-    sigtype.add_argument('-ss','--stringset', action='store_true',help='Operate using string set matching.')
 
-    parser.add_argument('-i','--infile', help = 'Input for generation or matching. Can be a file or directory.')
-    parser.add_argument('-o','--outfile',help = 'An output file for generate mode when generating new signatures.')
+    sigtype.add_argument('-z','--zigs', 
+        action='store_true',help='Operate using r2 zignature format.')
+    sigtype.add_argument('-ss','--stringset', 
+        action='store_true',help='Operate using string set matching.')
+
+    parser.add_argument('-i','--infile', 
+        help = 'Input for generation or matching. Can be a file or directory.')
+    parser.add_argument('-o','--outfile',
+        help = 'An output file for generate mode when generating new signatures.')
 
     args = parser.parse_args()
 
@@ -379,7 +355,7 @@ if __name__ == '__main__':
 
     if args.generate and args.zigs and args.infile and args.outfile:
 
-        zh= ZigHandler()
+        zh = ZigHandler()
         zh.generate_hashes_from_input(args.infile, args.outfile)
 
     elif args.match and args.zigs and args.infile:
