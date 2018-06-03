604df5261c6bf4f222ba06c39fb4fc8ee9b27206
Matt Brooks <cmatthewbrooks@gmail.com>
2018-06-03 17:31:57 +0800

    Update funclist.py to use new r2utils design

--- funclist.py
+++ funclist.py
@@ -5,64 +5,58 @@ import os,sys
 import argparse
 
 import r2pipe
-import r2utils as r2u
 
-r2utils = r2u.R2Utils()
+from r2utils import R2PipeUtility as r2pu, R2FuncUtility as r2fu
 
 class FuncList:
 
-    def __init__(self, list_type, file=None):
+    def __init__(self, list_type, input_obj=None):
 
         self.func_list = set()
         self.list_type = list_type
-        self.file = file
+        self.input_obj = r2pu.get_analyzed_r2pipe_from_input(input_obj)
 
-        self.populate_list(self.list_type, self.file)
-
-    # The next two methods are the init method and only external
-    # method. Everything below the '###' line is internal-only.
+        self.populate_list(self.list_type, self.input_obj)
 
     def populate_list(self, list_type, file=None):
 
         if list_type == 'firstround':
-            self.func_list.update(self.get_first_round_list(file))
+            self.func_list.update(self.get_first_round_list())
         elif list_type == 'utility':
-            self.func_list.update(self.get_utility_list(file))
-        
-    def print_functions(self):
+            self.func_list.update(self.get_utility_list())
 
-        for func in sorted(self.func_list):
-            print func
-
-    ####################################################################
-
-    def get_first_round_list(self, file=None):
+    def get_first_round_list(self):
 
         first_round_funcs = []
 
-        funcj_list = r2utils.get_funcj_list(file)
+        funcj_list = r2pu.get_funcj_list(self.input_obj)
 
         for funcj in funcj_list:
 
-            if (funcj['name'].startswith('fcn.') and 
-                r2utils.check_is_first_round_func(funcj)):
+            if (funcj['name'].startswith('fcn.') and
+                r2fu.check_is_first_round_func(funcj)):
                 first_round_funcs.append(funcj['name'])
-        
+
         return first_round_funcs
 
-    def get_utility_list(self, file=None):
+    def get_utility_list(self):
 
         utility_funcs = []
 
-        funcj_list = r2utils.get_funcj_list(file)
+        funcj_list = r2pu.get_funcj_list(self.input_obj)
 
         for funcj in funcj_list:
 
-            if (funcj['name'].startswith('fcn.') and 
-                r2utils.check_is_utility_func(funcj)):
+            if (funcj['name'].startswith('fcn.') and
+                r2fu.check_is_utility_func(funcj)):
                 utility_funcs.append(funcj['name'])
 
         return utility_funcs
+
+    def print_functions(self):
+
+            for func in sorted(self.func_list):
+                print func
 
 
 def usage():
@@ -108,29 +102,24 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     list_type = parser.add_mutually_exclusive_group(required=True)
+
     list_type.add_argument('-u','--utility',action='store_true',
         help='Print the utility functions (used by 3 or more functions)')
     list_type.add_argument('-fr','--firstround',action='store_true',
-        help='Print the first-round functions that do not call other functions.')
+        help='Print the first-round functions (no call instructions)')
 
-    parser.add_argument('-f','--file',help='The utility requires a binary to parse.')
+    parser.add_argument('-f','--file',
+        help='The utility requires a binary to parse.')
 
     args = parser.parse_args()
 
 
-    if args:
-        
-        if args.utility:
-            fl = FuncList('utility',args.file)
-        elif args.firstround:
-            fl = FuncList('firstround',args.file)
-        else:
-            print '\nCannot execute this list type.\n'
-            sys.exit(1)
-
-        fl.print_functions()
-
+    if args.utility:
+        fl = FuncList('utility',args.file)
+    elif args.firstround:
+        fl = FuncList('firstround',args.file)
     else:
-
-        print '\nCannot process file \'%s\'.\n' % args.file
+        print '\nCannot execute this list type.\n'
         sys.exit(1)
+
+    fl.print_functions()
